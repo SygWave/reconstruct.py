e375867285d43e3b0a794116a72fbe1b1efc9fde
Michael A. Agarenzo <magarenzo@gmail.com>
2020-10-21 14:56:57 -0400

    Simplify logic

--- src/reconstruct.py
+++ src/reconstruct.py
@@ -2,43 +2,36 @@ import sys
 
 # Reconstruct social links to privacy-friendly front-ends
 def reconstruct(url: str) -> str:
+    com = ".com/"
 
-    # If url is not a non-empty str
-    if url:
+    # If com in url
+    if com in url:
         instagram = "instagram"
         twitter = "twitter"
         youtube = "youtube"
-        com = ".com/"
+        platform = url.split(com)[0]
+        platform_lower = platform.lower()
+        subdirectory = url.split(com)[1]
+        key = url.split(platform + com)[1]
 
-        # If com in url
-        if com in url:
-            platform = url.split(com)[0]
-            platform_lower = platform.lower()
-            subdirectory = url.split(com)[1]
-            key = url.split(platform + com)[1]
+        # Instagram -> Bibliogram
+        # If instagram in platform_lower and "p/" in subdirectory then url is treated as Instagram profile...
+        if instagram in platform_lower and "p/" in subdirectory:
+            return "https://bibliogram.art/" + key
 
-            # Instagram -> Bibliogram
-            # If instagram in platform_lower and "p/" in subdirectory then url is treated as Instagram profile...
-            if instagram in platform_lower and "p/" in subdirectory:
-                return "https://bibliogram.art/" + key
+        # ...Else if instagram in platform_lower then url is treated as Instagram post
+        elif instagram in platform_lower:
+            return "https://bibliogram.art/u/" + key
 
-            # ...Else if instagram in platform_lower then url is treated as Instagram post
-            elif instagram in platform_lower:
-                return "https://bibliogram.art/u/" + key
+        # Twitter -> Nitter
+        # Else if twitter in platform_lower then url is treated as Twitter profile or tweet
+        elif twitter in platform_lower:
+            return "https://nitter.net/" + key
 
-            # Twitter -> Nitter
-            # Else if twitter in platform_lower then url is treated as Twitter profile or tweet
-            elif twitter in platform_lower:
-                return "https://nitter.net/" + key
-
-            # YouTube -> Invidious
-            # Else if youtube in platform_lower then url is treated as YouTube channel or video
-            elif youtube in platform_lower:
-                return "https://invidious.site/" + key
-
-            # Else return empty str
-            else:
-                return ""
+        # YouTube -> Invidious
+        # Else if youtube in platform_lower then url is treated as YouTube channel or video
+        elif youtube in platform_lower:
+            return "https://invidious.site/" + key
 
         # Else return empty str
         else:
